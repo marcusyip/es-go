981df739ed6a1f1c5e303a40fed2138eb3a94f39
Marcus Yip <long3981@gmail.com>
2022-05-02 13:26:22 +0800

    Add projector mock test

--- test/e2e/projector_e2e_test.go
+++ test/e2e/projector_e2e_test.go
@@ -9,38 +9,19 @@ import (
 	"github.com/es-go/es-go/es/database"
 	"github.com/segmentio/ksuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestE2e_Success(t *testing.T) {
-	testID := ksuid.New().String()
-	// Setup
-	config := es.NewConfig()
-	db := database.Connect()
-
-	eventRegistry := es.NewEventRegistry()
-	eventRegistry.Set("created_event", &CreatedEvent{})
-	eventRegistry.Set("completed_event", &CompletedEvent{})
-
-	repository := es.NewAggregateRepository(config, db, eventRegistry)
-
-	// Test
-	service := es.NewCommandService()
-	service.Register("create_command", NewCreateCommandHandler(repository))
-	service.Register("complete_command", NewCompleteCommandHandler(repository))
-
-	var command es.Command
-	command = &CreateCommand{TransactionID: testID, Currency: "BTC", Amount: 1.11}
-	err := service.Execute(context.Background(), command)
-	assert.NoError(t, err)
-
-	command = &CompleteCommand{TransactionID: testID, DoneBy: "marcusyip"}
-	err = service.Execute(context.Background(), command)
-	assert.NoError(t, err)
-
-	fmt.Println("TestE2e_Success: done")
+type MockProjector struct {
+	mock.Mock
 }
 
-func TestE2e_FailInvalidCommand(t *testing.T) {
+func (m *MockProjector) Handle(ctx context.Context, event es.Event) error {
+	args := m.Called(ctx, event)
+	return args.Error(0)
+}
+
+func TestE2e_Projector_Success(t *testing.T) {
 	testID := ksuid.New().String()
 	// Setup
 	config := es.NewConfig()
@@ -61,11 +42,53 @@ func TestE2e_FailInvalidCommand(t *testing.T) {
 	service.Register("complete_command", NewCompleteCommandHandler(repository))
 
 	var command es.Command
-	command = &CreateCommand{TransactionID: testID, Currency: "", Amount: 1.11}
+	command = &CreateCommand{TransactionID: testID, Currency: "BTC", Amount: 1.11}
 	err := service.Execute(context.Background(), command)
-	assert.Error(t, err, "should return invalid command error")
+	assert.NoError(t, err)
+	// TODO: assert transaction_views table
+
+	command = &CompleteCommand{TransactionID: testID, DoneBy: "marcusyip"}
+	err = service.Execute(context.Background(), command)
+	assert.NoError(t, err)
 
 	// TODO: assert transaction_views table
 
-	fmt.Println("TestE2e_FailInvalidCommand: done")
+	fmt.Println("TestE2e_SuccessProjector: done")
+}
+
+func TestE2e_MockProjector_Success(t *testing.T) {
+	testID := ksuid.New().String()
+	// Setup
+	config := es.NewConfig()
+	db := database.Connect()
+
+	eventRegistry := es.NewEventRegistry()
+	eventRegistry.Set("created_event", &CreatedEvent{})
+	eventRegistry.Set("completed_event", &CompletedEvent{})
+
+	repository := es.NewAggregateRepository(config, db, eventRegistry)
+
+	mockProjector := new(MockProjector)
+	mockProjector.On("Handle", mock.Anything, mock.Anything).Return(nil)
+
+	repository.Subscribe("completed_event", mockProjector)
+
+	// Test
+	service := es.NewCommandService()
+	service.Register("create_command", NewCreateCommandHandler(repository))
+	service.Register("complete_command", NewCompleteCommandHandler(repository))
+
+	var command es.Command
+	command = &CreateCommand{TransactionID: testID, Currency: "BTC", Amount: 1.11}
+	err := service.Execute(context.Background(), command)
+	assert.NoError(t, err)
+	// TODO: assert transaction_views table
+
+	command = &CompleteCommand{TransactionID: testID, DoneBy: "marcusyip"}
+	err = service.Execute(context.Background(), command)
+	assert.NoError(t, err)
+
+	// TODO: assert transaction_views table
+
+	fmt.Println("TestE2e_SuccessMockEventHandler: done")
 }
