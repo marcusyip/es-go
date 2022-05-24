51ed02f5d87cdeb9354dfcf41010b3c7cba5e015
Marcus Yip <long3981@gmail.com>
2022-05-24 08:42:52 +0800

    Test with event data assertion

--- test/e2e/command_service_test.go
+++ test/e2e/command_service_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 
 	"github.com/es-go/es-go/es"
@@ -41,15 +42,46 @@ var _ = Describe("CommandService", func() {
 
 	Context("Commit", func() {
 		It("should not return error", func() {
+			// 1. Execute CreateCommand
 			var command es.Command
 			command = &CreateCommand{TransactionID: testID, Currency: "BTC", Amount: 1.11}
 			err := commandService.Execute(context.Background(), command)
 			Expect(err).NotTo(HaveOccurred())
-			// TODO: assert transaction_views table
 
+			// 2. Assert version 1 created_event
+			var events []*es.EventModel
+			events, err = aggregateRepository.ListEventsByAggregateIDVersion(context.Background(), testID, 0)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(events).To(HaveLen(1))
+			event1 := events[0]
+			Expect(event1.Version).To(Equal(1))
+			Expect(event1.EventType).To(Equal("created_event"))
+			var payload1 map[string]any
+			_ = json.Unmarshal([]byte(event1.Payload), &payload1)
+			Expect(payload1).To(Equal(map[string]any{
+				"currency": "BTC",
+				"amount":   1.11,
+			}))
+			Expect(event1.CreatedAt).ToNot(BeNil())
+
+			// 3. Execute CompleteCommand
 			command = &CompleteCommand{TransactionID: testID, DoneBy: "marcusyip"}
 			err = commandService.Execute(context.Background(), command)
 			Expect(err).NotTo(HaveOccurred())
+
+			// 4. Assert version 2 completed_event
+			events, err = aggregateRepository.ListEventsByAggregateIDVersion(context.Background(), testID, 1)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(events).To(HaveLen(1))
+			event2 := events[0]
+			Expect(event2.Version).To(Equal(2))
+			Expect(event2.EventType).To(Equal("completed_event"))
+			var payload2 map[string]any
+			_ = json.Unmarshal([]byte(event2.Payload), &payload2)
+			Expect(payload2).To(Equal(map[string]any{
+				"done_by": "marcusyip",
+			}))
+			Expect(event2.CreatedAt).ToNot(BeNil())
 		})
 	})
 
